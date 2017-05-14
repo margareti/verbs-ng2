ed7146a1a283b0489e27c1a03f54e0f2fa408823
Gary van Woerkens <gvanwoerkens@santech.fr>
2017-05-14 20:08:05 +0300

    change verbs data struct

--- src/app/components/options/options.component.ts
+++ src/app/components/options/options.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-// import './verbs.json';
-// var verbs = require('./verbs.json');
+import { Component, OnInit } from '@angular/core';
+import { Verb } from '../../interfaces/verb';
+import Verbs from '../../services/verbs';
+
 @Component({
-  selector: 'options',
-  templateUrl: './options.component.html',
-  // styleUrls: ['./app.component.css']
+    // selector: 'options',
+    templateUrl: './options.component.html',
+    // styleUrls: ['./app.component.css']
 })
-export class OptionsComponent {
-  title = 'Options component!';
+
+export class OptionsComponent implements OnInit {
+    // title = 'Options component!';
+    private verbs:[Verb];
+
+    ngOnInit() {
+        this.verbs = Verbs.get();
+        console.log('this.verbs:', this.verbs);
+    }
 }
