6b5457e6c2865809a49ad220973db3410d49b456
Gary van Woerkens <gvanwoerkens@santech.fr>
2017-05-14 17:45:40 +0300

    add verbs json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import verbs from './verbs.json';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+    title = 'app works!';
+
+    constructor() {
+        if (!localStorage.getItem('verbs')) {
+            localStorage.setItem('verbs', verbs);
+        }
+    }
 }
