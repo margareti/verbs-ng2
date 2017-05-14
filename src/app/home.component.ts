a30500e8cd8ce5f779576bf2dd751ac9be6b4424
Margarita I <margaret.vegan@gmail.com>
2017-05-14 16:45:40 +0300

    feat: latest updates

---
+++ src/app/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+// import './verbs.json';
+// var verbs = require('./verbs.json');
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  // styleUrls: ['./app.component.css']
+})
+export class HomeComponent {
+  title = 'Hooome works :)!';
+  // console.log(verbs);
+
+}
