cab1549a1f66f4ea604d53221863fbbbfa7a845a
Margarita I <margaret.vegan@gmail.com>
2017-05-27 16:28:11 +0300

    feat: failed attempt at focus optimisation

--- src/common/focus.directive.ts
+++ src/common/focus.directive.ts
@@ -1,25 +1,18 @@
 import {Directive, OnInit, ElementRef, Renderer} from '@angular/core';
 
 @Directive({selector: '[focus]'})
-export class FocusDirective  {
+export class FocusDirective implements OnInit {
 
   el:ElementRef;
   renderer:Renderer;
-  private _autofocus;
     constructor(el: ElementRef, renderer: Renderer) {
       this.renderer = renderer;
       this.el = el;
     }
 
     ngOnInit():any {
-      console.log("on init")
-      // if (this.el.nativeElement) {
-      //   this.renderer.invokeElementMethod(this.el.nativeElement, 'focus', []);
-      // }
-    }
-    ngAfterViewInit() {
-      console.log("view after init")
-        if (this._autofocus || typeof this._autofocus === "undefined")
-            this.renderer.invokeElementMethod(this.el.nativeElement, 'focus', []);
+      if (this.el.nativeElement) {
+        this.renderer.invokeElementMethod(this.el.nativeElement, 'focus', []);
+      }
     }
   }
--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChildren, OnInit, AfterViewInit, Renderer, ElementRef/*, Input*/ } from '@angular/core';
+import { Component, OnInit/*, Input*/ } from '@angular/core';
 // import { Router } from '@angular/router';
 
 import Games from '../../services/games';
@@ -23,12 +23,9 @@ export class GamePage {
     title:string = 'GAME';
     selectedTense:string = '';
     resultPage: ResultPage;
-    renderer:Renderer;
-
-    @ViewChildren('game') inputs;
 
 
-    constructor(renderer: Renderer/*private router: Router*/ ) {
+    constructor(/*private router: Router*/ ) {
         let game:Game = Games.getCurrentGame();
 
         if (!game) {
@@ -41,38 +38,8 @@ export class GamePage {
             this.game = game;
         }
 
-        this.renderer = renderer;
         this.tenses = this.getTenses(this.game.verbs);
         this.selectedTense = this.getRandomTense();
-    }
-
-    ngOnInit () {
-      console.log('inputs ', this.inputs);
-    }
-
-    ngAfterViewInit() {
-      console.log("after view init ", this.inputs);
-      // this.inputs.toArray().find((e) => {
-      //   return e.nativeElement.getAttribute('name') == 'response';
-      // }).nativeElement.focus();
-      // this.inputs.toArray().each(e => {
-      //   e.nativeElement.focus();
-      // })
-      console.log('array ', this.inputs.toArray()[0].nativeElement)
-      // this.inputs.toArray()[0].nativeElement.focus();
-
-      // this.inputs.changes.subscribe(children => {
-      //   console.log('children ', children);
-      //   children.last.nativeElement.focus();
-      // });
-      var renderer = this.renderer;
-      var inputs = this.inputs;
-      setTimeout(function() {
-        console.log("timeout 5sec");
-        inputs.toArray()[0].nativeElement.focus();
-        // renderer.invokeElementMethod(inputs.toArray()[0].nativeElement, 'focus', []);
-      }, 5000);
-
     }
 
     private getTenses(verbs:Verb[]):string[] {
